107f9403912b984ac4e96a252e1612cc2042e59b
yulialitina <102325485+yulialitina@users.noreply.github.com>
2022-03-24 23:23:44 +0300

    Create yulialit3.js

---
+++ yulialit3.js
@@ -0,0 +1,34 @@
+@@ -1,127 +1,127 @@
+<!doctype html>
+<html lang="en-us">
+	<head>
+		<meta charset="utf-8">
+		<title>{{ name }}</title>
+  <head>
+    <meta charset="utf-8">
+    <title>{{ name }}</title>
+
+		<link rel="icon" type="image/ico" href="{{ baseUrl }}/-/static/favicon.png"/>
+		<link rel="stylesheet" type="text/css" href="{{ baseUrl }}/-/static/main.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+	<body class="body">
+		<header class="main-header">
+			<nav class="navbar navbar-default red-bg white no-border no-rnd-cnr" role="navigation">
+				<div class="container">
+					<div class="navbar-header clearfix">
+						<div class="npm-logo brand">
+							<a href="{{ baseUrl }}"></a>
+						</div>
+    <link rel="icon" type="image/ico" href="{{ baseUrl }}/-/static/favicon.png"/>
+    <link rel="stylesheet" type="text/css" href="{{ baseUrl }}/-/static/main.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+  <body class="body">
+    <header class="main-header">
+      <nav class="navbar navbar-default red-bg white no-border no-rnd-cnr" role="navigation">
+        <div class="container">
+          <div class="navbar-header clearfix">
+            <div class="npm-logo brand">
+              <a href="{{ baseUrl }}"></a>
+            </div>
